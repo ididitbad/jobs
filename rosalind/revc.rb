94d1d802f9a7eab2f44baab3cf5cfd7a4b2e2173
ididitbad <ididitbad@gmail.com>
2017-06-03 22:09:47 +0300

    Update revc.rb

--- rosalind/revc.rb
+++ rosalind/revc.rb
@@ -1,4 +1,4 @@
-# usage:	[another program output | ] revc.rb [ < file]		
+# usage:	[another program output | ] ruby revc.rb [ < file]		
 
 ARGF.each do |i|
 	puts(i.to_s.gsub(/[ATGC]/, "A" => "T", "T" => "A", "G" => "C", "C" => "G").reverse)
