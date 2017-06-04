05807079de61639b85eb9b00614a734ab530b462
ididitbad <ididitbad@gmail.com>
2017-06-04 20:45:20 +0300

    Update star_pyramid.rb

--- itmoprog/star_pyramid.rb
+++ itmoprog/star_pyramid.rb
@@ -1,11 +1,8 @@
 while true
 	puts("Enter pyramid's height:")
 	height = Integer(gets.chomp) rescue -1
-	if height >= 1 and height <= 20
-		break
-	else
-		puts("Height should be an integer between 1 and 20")
-	end
+	break if height >= 1 and height <= 20
+	puts("Height should be an integer between 1 and 20")
 end
 (1..height).reverse_each do |i| 
 	puts(" " * (height - i) + "*" * (2 * i - 1))
